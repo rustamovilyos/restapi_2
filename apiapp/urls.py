bab2d419d941c8656de7b8e8ccda4e6a38240bd5
Ilyos Rustamov <rustamov7871@gmail.com>
2021-08-02 20:48:28 +0500

    restApi

---
+++ apiapp/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('members/', views.MemberList.as_view()),
+    path('members/<int:pk>', views.MemberDetail.as_view()),
+]
---
+++ apiapp/views.py
@@ -0,0 +1,43 @@
+from .models import Member
+from .serializers import MemberSerializer
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import generics, mixins
+
+
+# # THIS IS FIRST
+# class MemberList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+#     queryset = Member.objects.all()
+#     serializer_class = MemberSerializer
+# 
+#     def get(self, request):
+#         return self.list(request)
+# 
+#     def post(self, request):
+#         return self.create(request)
+# 
+# 
+# class MemberDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+#     queryset = Member.objects.all()
+#     serializer_class = MemberSerializer
+# 
+#     def get(self, request, pk):
+#         return self.retrieve(request, pk)
+# 
+#     def put(self, request, pk):
+#         return self.update(request, pk)
+# 
+#     def delete(self, request, pk):
+#         return self.destroy(request, pk)
+
+
+# NEXT STEP
+class MemberList(generics.ListCreateAPIView):
+    queryset = Member.objects.all()
+    serializer_class = MemberSerializer
+
+
+class MemberDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Member.objects.all()
+    serializer_class = MemberSerializer
